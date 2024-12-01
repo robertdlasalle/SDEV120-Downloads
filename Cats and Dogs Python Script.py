f27cfc2ff8a2332dfd4437c449088fbe9be642c9
robertdlasalle <158389881+robertdlasalle@users.noreply.github.com>
2024-12-01 12:06:12 -0500

    Add files via upload

---
+++ Cats and Dogs Python Script.py
@@ -0,0 +1,17 @@
+# Robert D. LaSalle
+# Module 6 Assignment - Fundamental concepts of Python - IDLE
+# SDEV 120
+# 12/02/2024
+
+
+# Set variable c to "cats"
+c = "cats"
+
+# Set variable d to "dogs"
+d = "dogs"
+
+# Sentence to be printed
+s = f"It is raining {c} and {d}!"  
+
+# Print the sentence
+print(s)
